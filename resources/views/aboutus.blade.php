ef57d912a07a08feb0dae90733fa8cb00d72c662
Sahil Sethi <sahilsethi688@gmail.com>
2025-12-23 17:54:33 +0530

    comment,function , for loop

--- resources/views/aboutus.blade.php
+++ resources/views/aboutus.blade.php
@@ -1,7 +1,22 @@
 <html>
+
+{{--
+this is a comment 
+this is a second comment --this is a blade comment
+--}}
+
+<!--this is a html comment-->  
     <body>
         <h1>About us</h1>
         <h2>Name: {{$name}}</h2>
         <h2>Email: {{$email}}</h2>
+
+        @for ($i=0;$i<10;$i++)
+            <p>{{ $i }}</p>
+
+         @if ($i==5)
+             <h1>this is a number {{ $i }}</h1>
+         @endif   
+        @endfor
     </body>
 </html>
